ad2c10115dbb2e88e5c9d34106df285507d915a8
masterkbh <godchiken@naver.com>
2018-08-03 15:10:44 +0900

    프로젝트구성 및 문제풀이

---
+++ src/main/java/kbh/com/practice/findDecimal/FindDecimal.java
@@ -0,0 +1,29 @@
+package kbh.com.practice.findDecimal;
+
+public class FindDecimal {
+
+    private int minNumber = 2;
+
+    public static void main(String[] args) {
+        System.out.println(new FindDecimal().solution(10));
+        System.out.println(new FindDecimal().solution(5));
+
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            boolean isDivision = true;
+            for (int j = minNumber; j < i; j++) {
+                if (i % j == 0) {
+                    isDivision = false;
+                }
+            }
+            if (isDivision == true) {
+                System.out.println(i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
