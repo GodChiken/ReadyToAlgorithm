c0286cd4b92ae141f6ee8df5474681c3110513f2
godchiken <funnic2@gmail.com>
2019-01-24 10:29:42 +0900

    예제추가

--- src/main/java/kbh/com/practice/etc/SumOfSequentialNumber.java
+++ src/main/java/kbh/com/practice/etc/SumOfSequentialNumber.java
@@ -1,44 +1,55 @@
 package kbh.com.practice.etc;
 
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class SumOfSequentialNumber {
     public static void main(String[] args) {
-        solution(100);
-        solution(101);
-        //너무 미미해서 overflow 가된다해도 테스트
-        System.out.println("==================================");
-        solution(1000000000);
-        solution(1000000001);
+        solution(100000000000L);
     }
-    public static void solution(int number){
+
+    public static void solution(Long number) {
         System.out.println("반복문으로 돌렸을 때");
         long loopStartTime = System.currentTimeMillis();
         System.out.println(getSumByLoop(number));
         long loopEndTime = System.currentTimeMillis();
-        System.out.println( "실행 시간 : " + ( loopEndTime - loopStartTime )+"\n");
+        System.out.println("실행 시간 : " + (loopEndTime - loopStartTime) + "\n");
 
         System.out.println("스트림으로 돌렸을 때");
         long streamStartTime = System.currentTimeMillis();
         System.out.println(getSumByStream(number));
         long streamEndTime = System.currentTimeMillis();
-        System.out.println( "실행 시간 : " + ( streamEndTime - streamStartTime )+"\n");
+        System.out.println("실행 시간 : " + (streamEndTime - streamStartTime) + "\n");
+
+        System.out.println("병렬스트림으로 돌렸을 때");
+        long parallelStreamStartTime = System.currentTimeMillis();
+        System.out.println(getSumByParallelStream(number));
+        long parallelStreamEndTime = System.currentTimeMillis();
+        System.out.println("실행 시간 : " + (parallelStreamEndTime - parallelStreamStartTime) + "\n");
 
         System.out.println("수식을 활용할 때");
         long calculateStartTime = System.currentTimeMillis();
         System.out.println(getSumByCalculate(number));
         long calculateEndTime = System.currentTimeMillis();
-        System.out.println( "실행 시간 : " + ( calculateEndTime - calculateStartTime )+"\n");
+        System.out.println("실행 시간 : " + (calculateEndTime - calculateStartTime) + "\n");
     }
-    public static int getSumByLoop(int number){
-        int sum = 0;
-        for (int i = 1; i <= number; i++) { sum+=i; }
+
+    public static Long getSumByLoop(Long number) {
+        long sum = 0;
+        for (long i = 1; i <= number; i++) {
+            sum += i;
+        }
         return sum;
     }
-    public static int getSumByStream(int number){
-        return IntStream.range(1,number+1).sum();
+
+    public static Long getSumByStream(Long number) {
+        return LongStream.range(1, number + 1).sum();
     }
-    public static int getSumByCalculate(int number){
-        return number%2==0 ? (number+1)*(number/2) : (number*(number-1)/2) + number;
+
+    public static Long getSumByParallelStream(Long number) {
+        return LongStream.range(1, number + 1).parallel().sum();
+    }
+
+    public static Long getSumByCalculate(Long number) {
+        return number % 2 == 0 ? (number + 1) * (number / 2) : (number * (number - 1) / 2) + number;
     }
 }
